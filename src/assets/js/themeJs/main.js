5e990e9c1d641ea63f8843650e89651b2d38e993
Kailash <kailash_kds@yahoo.com>
2023-04-04 16:58:40 +0530

    Plugin js saprated

--- src/assets/js/themeJs/main.js
+++ src/assets/js/themeJs/main.js
@@ -794,20 +794,20 @@
 	/**********************
 	*Price Slider
 	***********************/
-	$( "#slider-range" ).slider({
-		range: true,
-		min: 0,
-		max: 120,
-		values: [ 0, 120 ],
-		slide: function( event, ui ) {
-			$("#amount").val("$" + ui.values[0] + "  $" + ui.values[1]);
-		}
-	});
-    $("#amount").val("$" + $("#slider-range").slider("values", 0) + " - " +
-        "$" + $("#slider-range").slider("values", 1));
+	// $( "#slider-range" ).slider({
+	// 	range: true,
+	// 	min: 0,
+	// 	max: 120,
+	// 	values: [ 0, 120 ],
+	// 	slide: function( event, ui ) {
+	// 		$("#amount").val("$" + ui.values[0] + "  $" + ui.values[1]);
+	// 	}
+	// });
+    // $("#amount").val("$" + $("#slider-range").slider("values", 0) + " - " +
+    //     "$" + $("#slider-range").slider("values", 1));
 
 
-    $('.zoom').zoom();
+    // $('.zoom').zoom();
 
 
 	/**********************
